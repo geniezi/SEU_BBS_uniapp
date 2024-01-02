da0cbdf6b414cd8c14259434a8c8e63a72437b7c
chen-guanzhi <1324216068@qq.com>
2024-01-02 17:21:30 +0800

    聊天未登录跳转login

--- pages/module1/index.vue
+++ pages/module1/index.vue
@@ -2,12 +2,12 @@
 	<view>
 		<u-button class="refreshButton" text="123" @click="onScrollToUpper" size="mini">轻触刷新</u-button>
 		<view class="message-list" v-if="getStatus()==200">
-			<view v-for="(message, index) in messages" :key="index" class="message-item" @click="goToChat(message.opposeId)"
-			>
-			<!-- <img class="avatar":src='avatar' v-if="searchSenderName(message.opposeId)" alt="Avatar"> -->
+			<view v-for="(message, index) in messages" :key="index" class="message-item"
+				@click="goToChat(message.opposeId)">
+				<!-- <img class="avatar":src='avatar' v-if="searchSenderName(message.opposeId)" alt="Avatar"> -->
 				<!-- searchSenderName(message.opposeId) -->
-				<view class="avatar" >
-				<img class="avatar":src='getIconUrlById(message.opposeId)' alt="Avatar">
+				<view class="avatar">
+					<img class="avatar" :src='getIconUrlById(message.opposeId)' alt="Avatar">
 				</view>
 				<view class="content">
 					<view clsass="sender">{{ getUsernameById(message.opposeId)}}</view>
@@ -25,58 +25,63 @@
 <script>
 	export default {
 		onShow() {
-		  this.$myRequest({
-		    header: {
-		      'Authentication': uni.getStorageSync('Authentication')
-		    },
-		    url: '/chat/pagePeople',
-		    method: 'GET',
-		    data: {
-		      "page": 0,
-		      "size": 20,
-		    },
-		  })
-		  .then(res => {
-		    console.log("chat people search succ");
-		    this.messages = res.data.data.records;
-			this.status=res.data.code;
-			console.log(this.status)
-		
-		    // 在这里进行第二个请求，确保在第一个请求完成后执行
-		    const opposeIds = this.messages.map(message => message.opposeId);
-		    console.log(opposeIds);
-		    const urlWithIds = '/user/listNIByIds?id=' + opposeIds.join('&id=');
-		    console.log("url is" + urlWithIds);
-		
-		    return this.$myRequest({
-		      header: {
-		        'Authentication': uni.getStorageSync('Authentication')
-		      },
-		      url: urlWithIds,
-		      method: 'GET'
-		    });
-		  })
-		  .then(response => {
-		    this.ListInfo = response.data.data;
-		    console.log("list search succ1");
-		    console.log(this.ListInfo);
-		  })
-		  .catch(error => {
-		    console.error("Error occurred:", error);
-		  });
-		  
-	
+			this.$myRequest({
+					header: {
+						'Authentication': uni.getStorageSync('Authentication')
+					},
+					url: '/chat/pagePeople',
+					method: 'GET',
+					data: {
+						"page": 0,
+						"size": 20,
+					},
+				})
+				.then(res => {
+					console.log("chat people search succ");
+					this.messages = res.data.data.records;
+					this.status = res.data.code;
+					console.log(this.status)
+
+					// 在这里进行第二个请求，确保在第一个请求完成后执行
+					const opposeIds = this.messages.map(message => message.opposeId);
+					console.log(opposeIds);
+					const urlWithIds = '/user/listNIByIds?id=' + opposeIds.join('&id=');
+					console.log("url is" + urlWithIds);
+
+					return this.$myRequest({
+						header: {
+							'Authentication': uni.getStorageSync('Authentication')
+						},
+						url: urlWithIds,
+						method: 'GET'
+					});
+				})
+				.then(response => {
+					this.ListInfo = response.data.data;
+					console.log("list search succ1");
+					console.log(this.ListInfo);
+				})
+				.catch(error => {
+					console.error("Error occurred:", error);
+
+					setTimeout(() => {
+					    this.goToLogin();
+					}, 1000);
+
+				});
+
+
 		},
 
 
 
 		data() {
 			return {
-				avatar:'',
-				username:'',
-				status:0,
-				
-				ListInfo:[],
+				avatar: '',
+				username: '',
+				status: 0,
+
+				ListInfo: [],
 				messages: [{
 					opposeId: "",
 					content: "",
@@ -105,55 +110,77 @@
 		},
 
 		methods: {
-			onScrollToUpper(){
-			uni.reLaunch({
-				url: '/pages/module1/index'
-			})
+
+			goToLogin() {
+				// uni.switchTab({
+				// 	url: '/pages/login/index',
+				// 	success: () => {
+				// 		uni.$u.toast('请登录后操作');
+				// 	},
+				// 	fail: (res) => {
+				// 		console.log('navigate failed', res);
+				// 	}
+				// })
+				//login从tabbar取出后用
+				uni.navigateTo({
+					url: '/pages/login/index',
+					success: () => {
+						uni.$u.toast('请登录后操作');
+					},
+					fail: (res) => {
+						console.log('navigate failed', res);
+					}
+				});
 			},
-			
-			test(ccc){
+			onScrollToUpper() {
+				uni.reLaunch({
+					url: '/pages/module1/index'
+				})
+			},
+
+			test(ccc) {
 				console.log(123)
 				console.log(ccc)
 			},
-			
-			getStatus(){
+
+			getStatus() {
 				return this.status;
 			},
-			
-			
-			
+
+
+
 			// 根据 ID 查找 username
 			getUsernameById(id) {
-			  const foundData = this.ListInfo.find(item => item.id === id);
-			    return foundData ? foundData.username : null; // 如果找到匹配的ID则返回对应的username，否则返回null或默认值
+				const foundData = this.ListInfo.find(item => item.id === id);
+				return foundData ? foundData.username : null; // 如果找到匹配的ID则返回对应的username，否则返回null或默认值
 			},
-			
+
 			// 根据 ID 查找 iconUrl
 			getIconUrlById(id) {
-			   const foundData = this.ListInfo.find(item => item.id === id);
-			    return foundData ? foundData.iconUrl : null; // 如果找到匹配的ID则返回对应的iconUrl，否则返回null或默认值
+				const foundData = this.ListInfo.find(item => item.id === id);
+				return foundData ? foundData.iconUrl : null; // 如果找到匹配的ID则返回对应的iconUrl，否则返回null或默认值
 			},
-			
+
 			searchListInfo() {
-				const messages=this.messages;
+				const messages = this.messages;
 				console.log(messages)
 				// 提取 messages 中所有的 opposeId 组成数组
 				const opposeIds = messages.map(message => message.opposeId);
 				console.log(opposeIds)
 				// 将数组转换为包含多个 id 的 URL 形式
 				const urlWithIds = '/user/listNIByIds?id=' + opposeIds.join('&id=');
-				console.log("url is"+urlWithIds)
+				console.log("url is" + urlWithIds)
 				this.$myRequest({
 						header: {
 							'Authentication': uni.getStorageSync('Authentication')
 						},
 						url: urlWithIds,
 						method: 'GET',
-						
+
 					})
 					.then(response => {
 
-						this.ListInfo=response.data.data;
+						this.ListInfo = response.data.data;
 						console.log("list search succ1")
 						console.log(this.ListInfo)
 					});
@@ -167,19 +194,19 @@
 						url: '/user/getNI/' + senderid,
 						method: 'GET',
 					})
-					.then(response => { 
+					.then(response => {
 						console.log("chatParter avatar search succ")
 						return response.data.data.iconUrl;
 					});
 			},
 
 
-			
+
 			goToChat(userId) {
 				// 跳转到聊天界面，userId为用户ID
 				uni.navigateTo({
-					url: '/pages/module1/chat?id='+encodeURIComponent(userId),
-					
+					url: '/pages/module1/chat?id=' + encodeURIComponent(userId),
+
 				});
 				console.log(userId)
 			}
